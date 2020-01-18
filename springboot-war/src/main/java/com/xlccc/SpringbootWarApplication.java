e0eb3f32f85fd8de5d1323a1d9cdcfc48df14dbc
CCCMacro <macrofei@163.com>
2020-01-18 17:03:31 +0800

    springboot-war

--- springboot-war/src/main/java/com/xlccc/SpringbootWarApplication.java
+++ springboot-war/src/main/java/com/xlccc/SpringbootWarApplication.java
@@ -1,13 +1,19 @@
 package com.xlccc;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
- * 仅部署为war包方式运行
+ * 完美解决将文件部署为war包
+ * 并在工程内可以直接启动
  */
+
+@EnableAutoConfiguration
+@ComponentScan
 @SpringBootApplication
 public class SpringbootWarApplication extends SpringBootServletInitializer {
 

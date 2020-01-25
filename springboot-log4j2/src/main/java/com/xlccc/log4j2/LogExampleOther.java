2ed797527946e6f4440c960329a4a2c7680d1832
CCCMacro <44434429+MacroFei@users.noreply.github.com>
2020-01-26 02:15:49 +0800

    springboot-log4j2

---
+++ springboot-log4j2/src/main/java/com/xlccc/log4j2/LogExampleOther.java
@@ -0,0 +1,18 @@
+package com.xlccc.log4j2;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class LogExampleOther {
+//  private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(LogExampleOther.class);
+  
+  public static void main(String... args) {
+
+    log.error("Something else is wrong here");
+    log.trace("Spring boot启动初始化 trace");
+    log.debug("Spring boot启动初始化 debug");
+    log.info("Spring boot启动初始化 info");
+    log.warn("Spring boot启动初始化 warn");
+
+  }
+}
